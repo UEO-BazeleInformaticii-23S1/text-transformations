6cbb1c6297f2bc346ccb86683ec59446afe7d2f8
Florin Lazar <florin.lazar.it2014@gmail.com>
2024-01-15 09:44:38 +0200

    implement base structure

--- TextTransformations/Program.cs
+++ TextTransformations/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace TextTransformations
+﻿using TextTransformations.Library;
+
+namespace TextTransformations
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string text = "This is the original text";
+
+            string result = TransformationEngine.ApplyAll(text);
+
+            Console.WriteLine("Result=" + result);
         }
     }
 }
